696e5ab7c993274f05e5b9588f8751a9c44c34f9
UVBrain services <uvbrainsolutions@outlook.com>
2015-10-15 09:58:16 +0530

    First draft

---
+++ USBDriveClone/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Management;
+using System.Text;
+
+namespace USBDriveClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usbCloneManager = new USBDriveCloneManager();
+            var destinationDir = @"c:\temp\usb-data";
+            usbCloneManager.StartUSBCloneEngine(destinationDir);
+            Console.ReadLine();
+
+        }
+    }
+}
